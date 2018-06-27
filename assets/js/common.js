7124f3948e7240b3d6bd97a2029b3a9a2f1934ad
Mikhail Vikrhian <m.vihrian@afonico.ru>
2018-06-27 10:38:21 +0300

    Added and fiex testimonials, map-region

--- assets/js/common.js
+++ assets/js/common.js
@@ -28,6 +28,52 @@ $(document).ready( function(){
  } )
 
  /*
-  * 03
+  * 03 Parallax
 	*/
 	var rellax = new Rellax( '.rellax' )
+
+	/*
+	 * 04 Qty field
+	 */
+
+	 $( '.qty-plus' ).on( 'click', function(e){
+		 var $t = $( '#'+$(this).data('target') )
+		 var cv = $t.val()
+		 var min = $t.attr('min')
+		 var max = $t.attr('max')
+
+		 if( cv >= min && cv < max ) {
+			 cv++
+			 $t.val( cv )
+		 }
+	 } )
+
+	 $( '.qty-minus' ).on( 'click', function(e){
+		 var $t = $( '#'+$(this).data('target') )
+		 var cv = $t.val()
+		 var min = $t.attr('min')
+		 var max = $t.attr('max')
+
+		 if( cv > min && cv <= max ) {
+			 cv--
+			 $t.val( cv )
+		 }
+	 } )
+
+	 /*
+	  * 05 Files Upload
+		*/
+		$('.form-group--file .file-trigger').on('click', function(e){
+			e.preventDefault()
+			$(this).next().trigger('click')
+		})
+
+		$('.form-group--file input').on('change', function(e){
+			var name = getFileName( $(this).val() )
+			$(this).next().text( name )
+			$(this).next().addClass('not-empty')
+		})
+		//get file name from field attr
+		function getFileName( path ) {
+			return path.split(/(\\|\/)/g).pop()
+		}
