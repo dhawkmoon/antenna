efc57aa78a543a7d47abd1dcf3ba4abb2d72f5a7
Mikhail Vikrhian <m.vihrian@afonico.ru>
2018-07-08 23:46:49 +0300

    Mobile fixes

--- assets/js/tooltips.js
+++ assets/js/tooltips.js
@@ -5,31 +5,59 @@ jQuery(document).ready( function($){
 		return r.replace( ']', '</b>')
 
 	}
+	if( $( window ).width() >= 768 ) {
+		$( '[data-toggle="tooltip"]' ).on( 'mouseenter', function(){
 
-	$( '[data-toggle="tooltip"]' ).on( 'mouseenter', function(){
+			var text = format( $(this).data('text') )
+			var $tooltip = $('<span class="tooltip">'+text+'</span>')
 
+			var width =  $(this).data('width')  || 'auto'
+
+			$tooltip.width( width )
+
+			$tooltip.css( {
+				left: $(this).offset().left+'px',
+				top: $(this).offset().top+'px',
+			} )
+
+			if( $(this).data('position') == 'bottom' )
+				$tooltip.css({marginTop:$(this).height()+20})
+
+			$tooltip.css({marginLeft:$(this).width()/2})
+
+			$('body').append( $tooltip )
+			$tooltip.animate( {opacity: 1}, 200 )
+
+			$(this).on( 'mouseleave', {tooltip: $tooltip}, function(e){
+				//
+				e.data.tooltip.animate( {opacity: 0}, 200, function(){
+					$(this).remove()
+				} )
+				//
+			} )
+			$(this).on( 'click', {tooltip: $tooltip}, function(e){
+				//
+				e.data.tooltip.animate( {opacity: 0}, 200, function(){
+					$(this).remove()
+				} )
+				//
+			} )
+		} )
+	} else {
+	$( '[data-toggle="tooltip"]' ).on( 'click', function(){
 		var text = format( $(this).data('text') )
 		var $tooltip = $('<span class="tooltip">'+text+'</span>')
 
 		var width =  $(this).data('width')  || 'auto'
 
 		$tooltip.width( width )
-		if( $(window).width() <= 768 ) {
-			$tooltip.css( {
-				left: $(this).offset().left+'px',
-				top: ( $(this).offset().top+100 )+'px',
-			} )
-		}
-		else {
-			$tooltip.css( {
-				left: $(this).offset().left+'px',
-				top: $(this).offset().top+'px',
-			} )
-		}
+
 		$tooltip.css( {
 			left: $(this).offset().left+'px',
-			top: $(this).offset().top+'px',
+			top: Math.abs( $(this).offset().top+$('body').scrollTop() )+'px',
 		} )
+
+
 
 		if( $(this).data('position') == 'bottom' )
 			$tooltip.css({marginTop:$(this).height()+20})
@@ -37,22 +65,25 @@ jQuery(document).ready( function($){
 		$tooltip.css({marginLeft:$(this).width()/2})
 
 		$('body').append( $tooltip )
-		$tooltip.animate( {opacity: 1}, 200 )
+		$tooltip.animate( {opacity: 1}, 200, function(){
+			$('body').on( 'click', {tooltip: $tooltip}, function(e){
+				//
+				e.data.tooltip.animate( {opacity: 0}, 200, function(){
+					$(this).remove()
+				} )
+				//
+			} )
+		} )
 
-		$(this).on( 'mouseleave', {tooltip: $tooltip}, function(e){
-			//
-			e.data.tooltip.animate( {opacity: 0}, 200, function(){
-				$(this).remove()
-			} )
-			//
-		} )
-		$(this).on( 'click', {tooltip: $tooltip}, function(e){
-			//
-			e.data.tooltip.animate( {opacity: 0}, 200, function(){
-				$(this).remove()
-			} )
-			//
-		} )
+
+		// $(this).on( 'click', {tooltip: $tooltip}, function(e){
+		// 	//
+		// 	e.data.tooltip.animate( {opacity: 0}, 200, function(){
+		// 		$(this).remove()
+		// 	} )
+		// 	//
+		// } )
 	} )
+}
 
-} )
+} ) //end document ready
